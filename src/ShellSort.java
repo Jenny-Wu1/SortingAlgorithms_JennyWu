d435f167db73282776baece0f7b205505153e7f9
Jenny Wu <jennywu22553@gmail.com>
2025-02-02 17:41:03 -0600

    Implemented shell sort functionality

--- src/ShellSort.java
+++ src/ShellSort.java
@@ -1,2 +1,20 @@
-public class ShellSort {
+import java.util.Arrays;
+
+public class ShellSort implements SortingAlgorithms {
+    @Override
+    public int[] sorty(int[] input) {
+        int n = input.length;
+        int temp;
+        int j;
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i++) {
+                temp = input[i];
+                for (j = i; j >= gap && temp < input[j - gap]; j -= gap) {
+                    input[j] = input[j - gap];
+                }
+                input[j] = temp;
+            }
+        }
+        return input;
+    }
 }
