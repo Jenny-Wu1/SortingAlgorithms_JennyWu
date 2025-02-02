d80c87b2d4e792d45cdeff1727787dbfa90a060e
Jenny Wu <jennywu22553@gmail.com>
2025-02-02 17:00:06 -0600

    Implemented insertion sort functionality

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,2 +1,17 @@
-public class InsertionSort {
+import java.util.Arrays;
+
+public class InsertionSort implements SortingAlgorithms {
+    @Override
+    public int[] sorty(int[] input) {
+        int temp;
+        int j;
+        for (int i = 1; i < input.length; i++) {
+            temp = input[i];
+            for (j = i; j > 0 && temp < input[j - 1]; j--) {
+                input[j] = input[j - 1];
+            }
+            input[j] = temp;
+        }
+        return input;
+    }
 }
