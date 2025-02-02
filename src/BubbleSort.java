040e4daee983019bfd4790e3a5697f29004eeac7
Jenny Wu <jennywu22553@gmail.com>
2025-02-02 16:41:47 -0600

    Implemented bubble sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,2 +1,17 @@
-public class BubbleSort {
+public class BubbleSort implements SortingAlgorithms {
+    @Override
+    public int[] sorty(int[] input) {
+        int n = input.length;
+        int temp;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (input[j] > input[j + 1]) {
+                    temp = input[j];
+                    input[j] = input[j + 1];
+                    input[j + 1] = temp;
+                }
+            }
+        }
+        return input;
+    }
 }
