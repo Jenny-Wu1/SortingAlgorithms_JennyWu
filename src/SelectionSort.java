b851c3063c02fa7fb887e6c7b3b9d2c62cf70b53
Jenny Wu <jennywu22553@gmail.com>
2025-02-02 17:19:17 -0600

    Implemented selection sort functionality

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,2 +1,19 @@
-public class SelectionSort {
+import java.util.Arrays;
+
+public class SelectionSort implements SortingAlgorithms {
+    @Override
+    public int[] sorty(int[] input) {
+        for (int i = 0; i < input.length; i++) {
+            int minIndex = i;
+            for (int j = i; j < input.length; j++) {
+                if (input[minIndex] > input[j]) {
+                    minIndex = j;
+                }
+            }
+            int temp = input[minIndex];
+            input[minIndex] = input[i];
+            input[i] = temp;
+        }
+        return input;
+    }
 }
