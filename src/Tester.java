ad25b2d6e60a05433754e6e10a5b0d01861b19e1
Jenny Wu <jennywu22553@gmail.com>
2025-02-02 20:40:50 -0600

    Added the singleTest method that creates an array of a given size, filling it randomly with numbers, running the sorting algorithm's sorty method, and records and returns the time it takes to sort the array

--- src/Tester.java
+++ src/Tester.java
@@ -1,6 +1,21 @@
+import java.util.Random;
+
 public class Tester {
     private SortingAlgorithms sa;
     public Tester(SortingAlgorithms sa) {
         this.sa = sa;
+    }
+
+    public double singleTest(int size) {
+        int[] input = new int[size];
+        Random gen = new Random();
+        for (int i = 0; i < size; i++) {
+            input[i] = gen.nextInt();
+        }
+
+        long startTime = System.nanoTime();
+        sa.sorty(input);
+        long endTime = System.nanoTime();
+        return endTime - startTime;
     }
 }
