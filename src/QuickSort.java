22f18d0ae88091677a2b38cc69b81845983ca819
Jenny Wu <jennywu22553@gmail.com>
2025-02-02 18:11:11 -0600

    Implemented quick sort functionality

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,2 +1,44 @@
-public class QuickSort {
+import java.util.Arrays;
+
+public class QuickSort implements SortingAlgorithms {
+    @Override
+    public int[] sorty(int[] input) {
+        quicksort(input, 0, input.length - 1);
+        return input;
+    }
+
+    private int partition(int[] input, int low, int high) {
+        int midpoint = low + (high - low) / 2;
+        int pivot = input[midpoint];
+        int temp;
+
+        boolean done = false;
+        while (!done) {
+            while (input[low] < pivot) {
+                low++;
+            }
+            while (pivot < input[high]) {
+                high--;
+            }
+            if (low >= high) {
+                done = true;
+            } else {
+                temp = input[low];
+                input[low] = input[high];
+                input[high] = temp;
+                low++;
+                high--;
+            }
+        }
+        return high;
+    }
+
+    private void quicksort(int[] input, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int lowEnd = partition(input, low, high);
+        quicksort(input, low, lowEnd);
+        quicksort(input, lowEnd + 1, high);
+    }
 }
