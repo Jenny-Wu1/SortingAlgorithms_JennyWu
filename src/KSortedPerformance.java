1f51c97477500d4187c7e396b3b62038e6ec6963
Jenny Wu <jennywu22553@gmail.com>
2025-02-06 16:46:53 -0600

    The performance class adjusted for the KSorted arrays

---
+++ src/KSortedPerformance.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+
+public class KSortedPerformance {
+    public static void main(String[] args) {
+        int[] sizes = {100, 500, 1000, 2000, 5000, 10000, 20000, 75000, 150000};
+        List<SortingAlgorithms> algorithms = Arrays.asList(
+                new BubbleSort(), new InsertionSort(), new SelectionSort(),
+                new ShellSort(), new QuickSort(), new MergeSort()
+        );
+        try (PrintStream fileOut = new PrintStream("KSorted_Performance_report.txt")) {
+            PrintStream consoleOut = System.out;
+            System.setOut(fileOut);
+
+            for (SortingAlgorithms algorithm : algorithms) {
+                System.out.println("Sorting algorithm - " + algorithm.getClass().getName());
+
+                KSortedTest tester = new KSortedTest(algorithm);
+                for (int size : sizes) {
+                    System.out.print("Sorted " + size + " elements in ");
+                    tester.test(20, size);
+                }
+                System.out.println();
+            }
+            System.setOut(consoleOut);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
